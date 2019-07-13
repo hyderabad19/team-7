36dafc9c850c51ee73b945dc5e5c9084497455b3
ruthvikacc <44341364+ruthvikacc@users.noreply.github.com>
2019-07-13 21:44:07 +0530

    Add files via upload
    Search analysis based on labels

--- PHP_Project/search_text.php
+++ PHP_Project/search_text.php
@@ -1,4 +1,12 @@
 <?php 
-$search_term = $_REQUEST["searchterm"];
-echo $search_term;
+require('db.php');
+$se=$_REQUEST["search"];
+//var $pdo;
+
+$data = $conn ->query("SELECT * FROM db1.uploadfile WHERE filename like '%$se%'")->fetchAll();
+// and somewhere later:
+foreach ($data as $row) {
+    echo $row['filename']."<br />\n";
+}
+
 ?>
